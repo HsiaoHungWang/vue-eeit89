16e336590d672725324dd99436a72876118ef851
HsioaHungWang <wang@ispan.com.tw>
2024-12-03 15:31:07 +0800

    分頁功能-介紹父子組件之前的資料傳遞

---
+++ src/components/PagingComponent.vue
@@ -0,0 +1,30 @@
+<script setup>
+    //接收父傳進來的資料
+    const props = defineProps({
+        total:Number,
+        thePage:Number
+    })
+
+    const emits = defineEmits(['goPaging'])
+
+    
+    const clickHandler=page=>{
+        //從子組件引發父組件的事件，並且傳了資料給父
+        emits('goPaging',page)
+    }
+</script>
+
+<template>
+  <nav aria-label="Page navigation example">
+  <ul class="pagination">
+   
+    <li @click="clickHandler(page)" class="page-item" v-for="page in total"><a class="page-link" href="#">{{ page }}</a></li>
+  
+   
+  </ul>
+</nav>
+</template>
+
+<style lang="css" scoped>
+
+</style>
--- src/views/SpotsView.vue
+++ src/views/SpotsView.vue
@@ -1,11 +1,13 @@
 <script setup>
+import PagingComponent from '@/components/PagingComponent.vue';
 import { ref, watchEffect } from 'vue';
 
 const BASE_URL = import.meta.env.VITE_APIURL
 const API_URL = `${BASE_URL}/spot/spotimages`
 const datas =ref({
- "categoryId" : 0, "keyword" : "北投", "start" : 1, "rows" : 9, "dir": false, "sort": "spotId"
+ "categoryId" : 0, "keyword" : "北投", "start" : 1, "rows" : 5, "dir": false, "sort": "spotId"
 })
+const totalPages = ref(0)
 
 const spots = ref({})
 
@@ -17,13 +19,23 @@ watchEffect(async()=>{
    })
    spots.value = await response.json()
    console.log(spots.value)
+
+   //計算共幾頁
+   totalPages.value = +datas.value.rows === 0 ? 1 : Math.ceil(spots.value.count / datas.value.rows)
+
 })
+
+//分頁
+const pagingHandler = page=>{
+    datas.value.start = page
+}
 
 </script>
 
 <template>
     <div>
 <h2>台北市景點</h2>
+<PagingComponent @goPaging="pagingHandler" :total="totalPages" :thePage="datas.start"></PagingComponent>
 <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
     <div class="col" v-for="{ spotId, spotImage, spotTitle, spotDescription, address } in spots.list" :key="spotId">
         <div class="card h-100">
